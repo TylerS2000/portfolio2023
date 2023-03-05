510abfa0fd9463c4dd65928563689de9ecbc506e
Tyler <schultty@mail.uc.edu>
2023-03-05 09:13:38 -0500

    changes in plans

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
-import Skills from './components/Skills';
 import Projects from './components/Projects';
 function App() {
   return (
     <div className="App">
       <Nav className="Nav"/>
       <About/>
-      <Skills/>
       <Projects/>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,31 @@
+import "./Projects.css";
 export default function Projects() {
+  const projects = [
+    {
+      name: 'Acctble.',
+      technology: 'React, Node, Express, MongoDB',
+      description: 'A social media app for sharing self improvements anonymously.',
+      image: 'https://via.placeholder.com/150',
+      link: 'https://google.com'
+    },
+    {
+      name: 'Project 2',
+      description: 'This is a project',
+      image: 'https://via.placeholder.com/150',
+      link: 'https://google.com'
+    }]
   return (
     <div className="Projects">
-        <div className="Project-Container">
-            <h3>Project 1</h3>
-            <p>Project 1 description</p>
-        </div>
+        {projects.map((project) => {
+          return (
+            <div className="Project">
+              <h3>{project.name}</h3>
+              <img src={project.image} alt={project.name}/>
+              <p>{project.technology}</p>
+              <p>{project.description}</p>
+              </div>
+          )
+        })}
     </div>
   );
 }
