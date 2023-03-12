e89de19d835adf5557a6e816fabcabe8d95db9ce
Tyler Schulten <tylerschulten@gmail.com>
2023-03-12 18:21:39 -0400

    beginnings of the end!!!

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,13 @@
 import "./Projects.css";
+import acctble from "./acctble.png";
+import wordle from "./wordle.png";
 export default function Projects() {
   const projects = [
     {
       name: 'Acctble.',
       technology: 'React, Node, Express, MongoDB',
       description: 'A social media app for sharing self improvements anonymously.',
-      image: "./acctble.png",
+      image: acctble,
       link: 'https://google.com',
       layout:""
     },
@@ -13,35 +15,48 @@ export default function Projects() {
       name: 'Wordle Clone',
       description: 'A clone of the popular word game.',
       technology: 'React, Typescript',
-      image: './acctble.png',
+      image: wordle,
+      link: 'https://google.com',
+      layout:"2"
+    },
+    {
+      name: 'Wordle Clone',
+      description: 'A clone of the popular word game.',
+      technology: 'React, Typescript',
+      image: wordle,
+      link: 'https://google.com',
+      layout:"2"
+    },
+    {
+      name: 'Wordle Clone',
+      description: 'A clone of the popular word game.',
+      technology: 'React, Typescript',
+      image: wordle,
       link: 'https://google.com',
       layout:"2"
     }]
   return (
     <div className="Projects">
-      <h1 className="title">My Work</h1>
-        <div className="container">
+      <div className="container">
+        <div>
         {projects.map((project) => {
           return (
-            <div className="Project row">
-              <div className={"img-container"+project.layout+" seven columns"}>
-              <img src={require("./acctble.png")} alt={project.name} />
+            <div className= "row">
+              <div className="project-container">
+              <div className="projectInfo six columns">
+                <h4><a href={project.link} style={{color:"black"}}>{project.name}</a></h4>
+                <p>{project.description}</p>
+                <p>{project.technology}</p>
+               
               </div>
-           
-              <div className={"text-container"+project.layout+" six columns"}>
-              <h2>{project.name}</h2>
-              <div className="description">
-              <p>Tools: {project.technology}</p>
-              <p>{project.description} made with {project.technology}</p>
-              </div>
-              <div className={`logo-container`+project.layout}>
-              <img src={require("./github.png")} alt={project.name} className="logo" />
-              <img src={require("./link.png")} alt={project.name} className="logo"/>
+              <div className="projectImage six columns">
+                <img src={project.image} alt="project" />
               </div>
               </div>
               </div>
-          )
+          );
         })}
+        </div>
         </div>
     </div>
   );
